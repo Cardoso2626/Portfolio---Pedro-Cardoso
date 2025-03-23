c5cb24df5dc2146c64e76166a1bb0a2842566c77
Cardoso2626 <sueleidesaraiva@hotmail.com>
2025-03-23 18:01:02 -0300

    Populando a página principal com as informações sobre o projeto

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -4,8 +4,18 @@ import MinhaFoto from "../img/minhaFoto.png"
 export default function Home(){
   return (
     <div>
-      <h1>OLÁ, ESTE É MEU PORTÓLIO</h1>
-      <Image src={MinhaFoto} alt="Foto do criador"/>
+      <div>
+        <h1>Bem vindos ao meu PORTFOLIO</h1>
+        <Image src={MinhaFoto} alt="Foto do criador"/>
+      </div>
+      <div>
+        <p>
+          Nesse portfólio mostro um pouco sobre mim e do que eu mais gosto de fazer, PROGRAMAÇÃO.
+          Acompanhe aqui: os conhecimentos das linguagens que possuo e alguns projetos que tenho, projetos em grupo, 
+          e pessoais. Esse portfolio será atualizado continuamente, de acordo com o avanço da minha carreira. 
+          FQUEM SEMPRE ATENTOS!
+        </p>
+      </div>
     </div>
   )
 }
