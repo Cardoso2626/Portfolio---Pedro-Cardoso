9769c44b136337baae6b9b1dba7036b55dd035c6
Cardoso2626 <sueleidesaraiva@hotmail.com>
2025-03-23 18:16:26 -0300

    Populando o footer de links a partir de icones de insta, git e linkedin

--- portfolio/src/components/Rodape/Rodape.tsx
+++ portfolio/src/components/Rodape/Rodape.tsx
@@ -1,7 +1,27 @@
+import IconeLinkeIn from "../../img/linkedInIcon.png"
+import IconeInstagram from "../../img/instaIcon.png"
+import IconeGithub from "../../img/githubIcon.png"
+import Link from "next/link"
+import Image from "next/image"
+
 export default function Rodape(){
     return(
         <footer>
-            <p>2025 Pedro Cardoso - Todos os direitos reservados</p>
+            <div>
+                <p>2025 Pedro Cardoso - Todos os direitos reservados</p>
+                <div>
+                    <p>Me acompanhe aqui:</p>
+                    <Link target={"_blank"} href={"https://www.linkedin.com/in/pedro-saraiva1/"}>
+                        <Image src={IconeLinkeIn} alt="Icone do LinkedIn"/>
+                    </Link>
+                    <Link target={"_blank"} href={"https://github.com/Cardoso2626"}>
+                        <Image src={IconeGithub} alt="Icone do GitHub"/>
+                    </Link>
+                    <Link target={"_blank"} href={"https://www.instagram.com/ph.saraivax/"}>
+                        <Image src={IconeInstagram} alt="Icone do Instagram"/>
+                    </Link>
+                 </div>
+            </div>
         </footer>
     )
 }
