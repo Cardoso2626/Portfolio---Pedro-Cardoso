cbfc700318c444bd5f628a1bb50576bbc9d4d4ca
Cardoso2626 <sueleidesaraiva@hotmail.com>
2025-03-26 13:53:28 -0300

    Nomeando as classes para fazer o flex

--- portfolio/src/components/Rodape/Rodape.tsx
+++ portfolio/src/components/Rodape/Rodape.tsx
@@ -6,20 +6,20 @@ import Image from "next/image"
 
 export default function Rodape(){
     return(
-        <footer>
-            <div>
-                <h3>Desenvolvido por Pedro Henrique!</h3>
-                <p>2025 Pedro Cardoso - Todos os direitos reservados</p>
-                <div>
-                    <p>Me acompanhe aqui:</p>
+        <footer className="containerFooter">
+            <h3 className="item item1">Desenvolvido por Pedro Henrique!</h3>
+            <p className="item item2">2025 Pedro Cardoso - Todos os direitos reservados</p>
+            <div className="item item3">
+                <p>Me acompanhe aqui:</p>
+                <div className="containerIcones">
                     <Link target={"_blank"} href={"https://www.linkedin.com/in/pedro-saraiva1/"}>
-                        <Image src={IconeLinkeIn} alt="Icone do LinkedIn"/>
+                        <Image src={IconeLinkeIn} alt="Icone do LinkedIn" className="icones"/>
                     </Link>
                     <Link target={"_blank"} href={"https://github.com/Cardoso2626"}>
-                        <Image src={IconeGithub} alt="Icone do GitHub"/>
+                        <Image src={IconeGithub} alt="Icone do GitHub" className="icones"/>
                     </Link>
                     <Link target={"_blank"} href={"https://www.instagram.com/ph.saraivax/"}>
-                        <Image src={IconeInstagram} alt="Icone do Instagram"/>
+                        <Image src={IconeInstagram} alt="Icone do Instagram" className="icones"/>
                     </Link>
                 </div>
             </div>
