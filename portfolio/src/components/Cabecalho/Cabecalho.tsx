87f6ab8a876740a6727a1090e6bf3e9721bdf7ac
Cardoso2626 <sueleidesaraiva@hotmail.com>
2025-03-26 14:18:32 -0300

    Terminando o footer colocando o display flex e arrumando itens

--- portfolio/src/components/Cabecalho/Cabecalho.tsx
+++ portfolio/src/components/Cabecalho/Cabecalho.tsx
@@ -3,8 +3,8 @@ import Menu from "../Menu/Menu";
 export default function Cabecalho(){
     return(
         <header className="containerCabecalho">
-            <h1 className="item item1 h1Cabecalho">PH.</h1>
-            <div className="item item2">
+            <h1 className="item1Cabecalho h1Cabecalho">PH.</h1>
+            <div className="item2Cabecalho">
                 <Menu />
             </div>
         </header>
--- portfolio/src/components/Rodape/Rodape.tsx
+++ portfolio/src/components/Rodape/Rodape.tsx
@@ -7,19 +7,19 @@ import Image from "next/image"
 export default function Rodape(){
     return(
         <footer className="containerFooter">
-            <h3 className="item item1">Desenvolvido por Pedro Henrique!</h3>
-            <p className="item item2">2025 Pedro Cardoso - Todos os direitos reservados</p>
-            <div className="item item3">
-                <p>Me acompanhe aqui:</p>
+            <h3 className="itemFoot item1">Desenvolvido por Pedro Henrique!</h3>
+            <p className="itemFoot item2">2025 Pedro Cardoso - Todos os direitos reservados</p>
+            <div className="itemFoot item3">
+                <p className="pFoot">Me acompanhe aqui:</p>
                 <div className="containerIcones">
                     <Link target={"_blank"} href={"https://www.linkedin.com/in/pedro-saraiva1/"}>
-                        <Image src={IconeLinkeIn} alt="Icone do LinkedIn" className="icones"/>
+                        <Image src={IconeLinkeIn} alt="Icone do LinkedIn" className="itemIcone icones"/>
                     </Link>
                     <Link target={"_blank"} href={"https://github.com/Cardoso2626"}>
-                        <Image src={IconeGithub} alt="Icone do GitHub" className="icones"/>
+                        <Image src={IconeGithub} alt="Icone do GitHub" className="itemIcone icones"/>
                     </Link>
                     <Link target={"_blank"} href={"https://www.instagram.com/ph.saraivax/"}>
-                        <Image src={IconeInstagram} alt="Icone do Instagram" className="icones"/>
+                        <Image src={IconeInstagram} alt="Icone do Instagram" className="itemIcone icones"/>
                     </Link>
                 </div>
             </div>
