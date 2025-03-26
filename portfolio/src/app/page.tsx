8e062c02a5e15b5b56d3165cd5af8ce51fbea84d
Cardoso2626 <sueleidesaraiva@hotmail.com>
2025-03-26 14:04:06 -0300

    Arrumando boas tags de boas práticas no page e acertando uma altura para

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -3,19 +3,22 @@ import MinhaFoto from "../img/minhaFoto.png"
 
 export default function Home(){
   return (
-    <div>
-      <div>
-        <h1>Bem vindos ao meu PORTFOLIO</h1>
-        <Image src={MinhaFoto} alt="Foto do criador"/>
-      </div>
-      <div>
-        <p>
-          Nesse portfólio mostro um pouco sobre mim e do que eu mais gosto de fazer, PROGRAMAÇÃO.
-          Acompanhe aqui: os conhecimentos das linguagens que possuo e alguns projetos que tenho, projetos em grupo, 
-          e pessoais. Esse portfolio será atualizado continuamente, de acordo com o avanço da minha carreira. 
-          FQUEM SEMPRE ATENTOS!
-        </p>
-      </div>
-    </div>
+    <main>
+      <section>
+        <div className="containerPrincipal">
+          <h2>Bem vindos ao meu PORTFOLIO</h2>
+          <Image src={MinhaFoto} alt="Foto do criador"/>
+        </div>
+        <div>
+          <p>
+            Nesse portfólio mostro um pouco sobre mim e do que eu mais gosto de fazer, PROGRAMAÇÃO.
+            Acompanhe aqui: os conhecimentos das linguagens que possuo e alguns projetos que tenho, projetos em grupo, 
+            e pessoais. Esse portfolio será atualizado continuamente, de acordo com o avanço da minha carreira. 
+            FIQUEM SEMPRE ATENTOS!
+          </p>
+        </div>
+      </section>
+    </main>
+
   )
 }
