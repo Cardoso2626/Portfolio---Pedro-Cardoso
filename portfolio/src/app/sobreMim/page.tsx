c7990c86b25f24a9f9aa73fa1b3b5e9047aaa139
Cardoso2626 <sueleidesaraiva@hotmail.com>
2025-03-27 11:07:12 -0300

    Dividindo a página de about em main e sections

--- portfolio/src/app/sobreMim/page.tsx
+++ portfolio/src/app/sobreMim/page.tsx
@@ -6,23 +6,29 @@ import IconeSql from "../../img/sQLIcon.png"
 
 export default function sbreMim (){
     return(
-        <div>
-            <div>
-                <h3>Cotando um pouco sobre mim</h3>
-                <p>
-                    Bom, meu nome é Pedro Cardoso, tenho 19 anos, e minha paixão com a programação surgiu muito por influência 
-                    mas também por parte individual. Sempre fiquei imaginando como cada página funcionava, e quando era menor eu ficava no computador
-                    vasculhando qualquer coisa que desse para clicar. Hoje, sigo com essa paixão, buscando conhecimentos, com muita força de vontade.
-                    Atualmente trabalho na Motorola Solutions Brasil como jovem aprendiz e faço faculdade na FIAP (Faculdadede Informática e Administração Paulista)
-                </p>
-            </div>
-            <div>
-                <h4>Essas são as linguagens que obetenho conhecimento: </h4>
-                <Image src={IconeJava} alt="Icone Java"/>
-                <Image src={IconeJavaScript} alt="Icone JS"/>
-                <Image src={IconePython} alt="Icone Python"/>
-                <Image src={IconeSql} alt="Icone SQL"/>
-            </div>
-        </div>
+        <main>
+            <section>
+                <div>
+                    <h3>Cotando um pouco sobre mim</h3>
+                    <p>
+                        Bom, meu nome é Pedro Cardoso, tenho 19 anos, e minha paixão com a programação surgiu muito por influência 
+                        mas também por parte individual. Sempre fiquei imaginando como cada página funcionava, e quando era menor eu ficava no computador
+                        vasculhando qualquer coisa que desse para clicar. Hoje, sigo com essa paixão, buscando conhecimentos, com muita força de vontade.
+                        Atualmente trabalho na Motorola Solutions Brasil como jovem aprendiz e faço faculdade na FIAP (Faculdadede Informática e Administração Paulista)
+                    </p>
+                </div>
+            </section>
+            <section>
+                <div>
+                    <h4>Essas são as linguagens que obetenho conhecimento: </h4>
+                    <Image src={IconeJava} alt="Icone Java"/>
+                    <Image src={IconeJavaScript} alt="Icone JS"/>
+                    <Image src={IconePython} alt="Icone Python"/>
+                    <Image src={IconeSql} alt="Icone SQL"/>
+                    
+                </div>
+            </section>
+        </main>
+        
     )
 }
