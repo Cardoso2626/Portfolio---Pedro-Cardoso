1fc01628824464edfe3c542aaec301cb48eee9af
Cardoso2626 <sueleidesaraiva@hotmail.com>
2025-03-31 18:24:26 -0300

    Adicionando imagens de certificado e tetaiva de fazer bum grid

--- portfolio/src/app/sobreMim/page.tsx
+++ portfolio/src/app/sobreMim/page.tsx
@@ -3,13 +3,21 @@ import IconeJavaScript from "../../img/jSIcon.png"
 import IconeJava from "../../img/javaIcon.png"
 import IconePython from "../../img/pythonIcon.png"
 import IconeSql from "../../img/sQLIcon.png"
+import Python1 from "../../img/py1.jpg"
+import Python2 from "../../img/py2.jpg"
+import Java from "../../img/java1.jpg"
+import JavaScript from "../../img/js1.jpg"
+import Senac1 from "../../img/senac1.jpg"
+import Senac2 from "../../img/senac2.jpg"
+import OneBit from "../../img/oneBit.jpg"
+
 
 export default function sbreMim (){
     return(
         <main>
             <section>
                 <div>
-                    <h3 className="h3SobreMim">Cotando um pouco sobre mim</h3>
+                    <h3 className="h3SobreMim">Contando um pouco sobre mim</h3>
                     <p className="pSobreMim">
                         Bom, meu nome é Pedro Cardoso, tenho 19 anos, e minha paixão com a programação surgiu muito por influência 
                         mas também por parte individual. Sempre fiquei imaginando como cada página funcionava, e quando era menor eu ficava no computador
@@ -25,6 +33,36 @@ export default function sbreMim (){
                     <Image src={IconeJavaScript} alt="Icone JS" className="iconesSobreMim"/>
                     <Image src={IconePython} alt="Icone Python" className="iconesSobreMim"/>
                     <Image src={IconeSql} alt="Icone SQL" className="iconesSobreMim"/>
+                </div>
+            </section>
+            <section>
+                <h4>Meus certificados...</h4>
+                <div className="containerCertificados">
+                    <div className="itemCertificado">
+                        <Image src={Java} alt="certificado Java" className="imgCertificado"/>
+                    </div>
+                    <div className="itemCertificado">
+                        <Image src={Python1} alt="certificado Python" className="imgCertificado"/>
+
+                    </div>
+                    <div className="itemCertificado">
+                        <Image src={Python2} alt="certificado Python" className="imgCertificado"/>
+                    </div >
+                    <div className="itemCertificado">
+                        <Image src={JavaScript} alt="certificado JavaScript"className="imgCertificado"/>
+                    </div>
+                    <div className="itemCertificado">
+                        <Image src={Senac1} alt="certificado Senac" className="imgCertificado"/>
+                    </div>
+                    <div className="itemCertificado">
+                        <Image src={Senac2} alt="certificado Senac" className="imgCertificado"/>
+
+                    </div>
+                    <div className="itemCertificado">
+                        <Image src={OneBit} alt="certificado OneBitCode" className="imgCertificado"/>
+                    </div>
+
+
                 </div>
             </section>
         </main>
