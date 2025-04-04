6a781935470f94915417ddf8c9183a41e46171f1
Cardoso2626 <sueleidesaraiva@hotmail.com>
2025-04-04 09:07:47 -0300

    Colocando icones de projetos mudando h2 da página

--- portfolio/src/app/projetos/page.tsx
+++ portfolio/src/app/projetos/page.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image"
+import Pc from "../../img/pcProjetos.png"
+
 export default function Projetos (){
     return(
         <main>
-            <h2>Esses são meus projetos</h2>
+            <h2 className="h2Projeto">Esses são meus projetos</h2>
+            <div>
+                <Image src={Pc} alt="Icone de pc" className="imagePc"/>
+                <Image src={Pc} alt="Icone de pc" className="imagePc"/>
+                <Image src={Pc} alt="Icone de pc" className="imagePc"/>
+            </div>
         </main>
     )
 }
