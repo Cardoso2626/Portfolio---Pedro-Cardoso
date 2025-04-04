794d1016b041e1e2b1baaa6dda40a8b569bafbf1
Cardoso2626 <sueleidesaraiva@hotmail.com>
2025-04-04 09:13:48 -0300

    Earth In balance

--- portfolio/src/app/projetos/page.tsx
+++ portfolio/src/app/projetos/page.tsx
@@ -1,15 +1,31 @@
 import Image from "next/image"
 import Pc from "../../img/pcProjetos.png"
+import Earth from "../../img/earthInBalance.png"
+import Link from "next/link"
 
 export default function Projetos (){
     return(
         <main>
-            <h2 className="h2Projeto">Esses são meus projetos</h2>
-            <div>
-                <Image src={Pc} alt="Icone de pc" className="imagePc"/>
-                <Image src={Pc} alt="Icone de pc" className="imagePc"/>
-                <Image src={Pc} alt="Icone de pc" className="imagePc"/>
-            </div>
+            <section>
+                <h2 className="h2Projeto">Esses são meus projetos</h2>
+                <div>
+                    <div>
+                    <Image src={Pc} alt="Icone de pc" className="imagePc"/>
+                    <Link href={"https://earth-in-balance.vercel.app/"} target="_blank" rel="noopener noreferrer">
+                        <Image src={Earth} alt="Página earth in balance" className="imgEarthIn"/>
+                    </Link>
+                    </div>
+                    <div>
+                    <Image src={Pc} alt="Icone de pc" className="imagePc"/>
+                    </div>
+                    <div>
+                    <Image src={Pc} alt="Icone de pc" className="imagePc"/>
+                    </div>
+                   
+                    
+                    
+                </div>
+            </section>
         </main>
     )
 }
